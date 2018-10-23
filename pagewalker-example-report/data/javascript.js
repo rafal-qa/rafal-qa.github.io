2c038f5fe62e035b46f6ea403c1c40ae525b63ed
rafal-qa <38441347+rafal-qa@users.noreply.github.com>
2018-10-23 21:51:57 +0200

    Page Walker v1.0.1 report

--- pagewalker-example-report/data/javascript.js
+++ pagewalker-example-report/data/javascript.js
@@ -1,3 +1,3 @@
 var data_javascript_head = ["id", "occurrences", "description", "pages_with_error"];
-var data_javascript_main = [];
-var data_javascript_stat = {"exception": 0};
+var data_javascript_main = [[1, 1, "TypeError: node.getAttribute is not a function at https://www.ncbi.nlm.nih.gov/tools/primer-blast/js/toggle.js:168:24 at Function.forEach (https://www.ncbi.nlm.nih.gov/tools/primer-blast/js/utils.js:525:19) at forEach (https://www.ncbi.nlm.nih.gov/tools/primer-blast/js/utils.js:545:17) at Function.Toggle.onload (https://www.ncbi.nlm.nih.gov/tools/primer-blast/js/toggle.js:166:4) at Toggle._onload (https://www.ncbi.nlm.nih.gov/tools/primer-blast/js/toggle.js:155:11)", [99]]];
+var data_javascript_stat = {"exception": 1};
