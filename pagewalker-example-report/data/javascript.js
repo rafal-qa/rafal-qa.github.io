94f97e82caeea9ffcaa229e0b6ad5bf9f9f947a5
rafal-qa <38441347+rafal-qa@users.noreply.github.com>
2019-06-16 19:49:37 +0200

    Page Walker v1.2.0 report

--- pagewalker-example-report/data/javascript.js
+++ pagewalker-example-report/data/javascript.js
@@ -1,3 +1,3 @@
 var data_javascript_head = ["id", "occurrences", "description", "pages_with_error"];
-var data_javascript_main = [];
-var data_javascript_stat = {"exception": 0};
+var data_javascript_main = [[1, 1, "Uncaught TypeError: node.getAttribute is not a function", [59]]];
+var data_javascript_stat = {"exception": 1};
